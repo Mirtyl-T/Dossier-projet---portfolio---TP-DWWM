984a1f540e08f90fabd670331305960f286efa69
Mirtyl-T <adrien.kahn98@gmail.com>
2025-10-01 15:10:01 +0000

    test api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-// Configuration pour le développement
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000/api' 
-};
-
-// Configuration pour la production
-// export const environment = {
-//   production: true,
-//   apiUrl: 'https://votre-api-production.com/api'
-=======
 // src/environments/environment.ts
 export const environment = {
   production: false,
@@ -20,5 +8,4 @@ export const environment = {
 // export const environment = {
 //   production: true,
 //   apiUrl: 'https://votre-domaine-production.com/api'
->>>>>>> master
 // };
